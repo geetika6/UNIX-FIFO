79034c4e88270808bbd74106a058a524b7852640
geetika6 <geetika6@gmail.com>
2016-07-27 12:41:04 +1000

    running only req client and serve for 1 fifo to debug step by step

--- CLIENT_SERVER/req_client.c
+++ CLIENT_SERVER/req_client.c
@@ -1,37 +1,77 @@
 #include "header.h"
-int main()
+int main(int argc, char **argv)
 {
 
-        int pipefd;
-        int pipefd1[2];
-        char buffer[BUFFER_SIZE+1];
-	int nbytes,stat,res,bytes_sent,result=0;
-	struct data *rdata;
-        rdata=&data_to_send;
+        int reqfd,resfd,client_fifo_fd,num_data;
+        char client_fifo[256];
+	int stat,res=0;
+        int *result[5];
+	struct data *rdata[5];
 	pid_t pid;
-        pipefd=open(FIFO,O_WRONLY);
-        printf("Process id =%d,fd =%d\n",getpid(),pipefd);
-        if (pipefd!=-1)
-        {
-        	while(bytes_sent< TEN_MEG)
-                {   
-  			res=write(pipefd,buffer,BUFFER_SIZE);
-			if(res==-1)
-			{
-				fprintf(stderr,"write on fifo not done\n");
-                	        exit(EXIT_FAILURE);
-			}
-                	bytes_sent+=res;
-                        printf("data written =%d\n",bytes_sent);
-		}
+        num_data=(int)(argv[1][0]-'0');
+        printf("num_data =%d\n",(int)(argv[1][0]-'0'));
 
-        	(void)close(pipefd);
+        for(int i=0;i<num_data;i++)
+        {
+        	rdata[i]=malloc(sizeof(struct data));
+		rdata[i]->first=rdata[i]->first+i;
+		rdata[i]->second=rdata[i]->first+rdata[i]->second+i;
+                if (i==5)
+			i=0;
+                rdata[i]->operator_name=operator_type[i];
+        }
+
+//MAKE client fifo with the process id in its name
+        sprintf(client_fifo,OP_CLIENT_FIFO,getpid());
+	stat=mkfifo(client_fifo,0777);
+        if(stat!=0)
+	{
+        	perror("make client_fifo");
+                exit(EXIT_FAILURE);
 	}
+//Open the request FIFO and place the various data packets with the respective operations        
+        reqfd=open(REQFIFO,O_WRONLY);
+        if (reqfd!=-1)
+        {
+        	int i=0;
+ 		while(1)
+        	{	
+
+        		printf("Process id =%d,data sent=%d and %d and operator sent=%c\n",getpid(),rdata[i]->first,rdata[i]->second,rdata[i]->operator_name);
+			if (write(reqfd,rdata[i],sizeof(struct data))==-1) 
+			{
+            			perror("write error");
+        	       	        exit(EXIT_FAILURE);
+       		 	}
+                        i++;
+                        if (i==num_data) 
+			{
+            			printf("Bye!\n");
+           			break;
+       			}
+
+
+		}
+	}
+
         else
         {
+                perror("req_fifo_open_client");
                 exit(EXIT_FAILURE);
 	
 	}
-        printf("Process id =%d,bytes sent=%d\n",getpid(),bytes_sent);
+//Now recieve the result of the operations
+//        client_fifo_fd=open(client_fifo,O_RDONLY);
+//        perror("client_fifo_open_result");
+//	if(client_fifo_fd!=-1)
+//	{	
+//		int j=0;
+//		while(j<2)
+//		{
+//			stat=read(client_fifo_fd,rdata[j],sizeof(struct data));
+//			printf("In client with process id=%d,result=%d\n",getpid(),rdata[j]->result);
+//  			j++;	
+//		}
+//	}
         exit(EXIT_SUCCESS);
 }
--- CLIENT_SERVER/server.c
+++ CLIENT_SERVER/server.c
@@ -1,53 +1,106 @@
 #include "header.h"
+int rc1, rc2;
+
+pthread_t thread1, thread2;
+struct data *rdata;
+int reqfd,pxfd,resfd,client_fifo_fd;
+char client_fifo[256];
+struct data *arr_data[100];
+
+
+void *read_req()
+{ 
+	int res,res1;
+	while(1)
+	{
+		if(res=read(reqfd,rdata,sizeof(struct data))>0)
+			printf("Server read data, first =%d,second=%d and operator=%c\n",rdata->first,rdata->second,rdata->operator_name);
+	}
+}
+//void *send_req()
+//{ 
+//	int res,res1;
+//	while(1)
+//	{
+//		res1=write(pxfd,rdata,sizeof(struct data));
+//		printf("Server Process data, first =%d,second=%d and operator=%c\n",rdata->first,rdata->second,rdata->operator_name);
+//	}
+//void *read_res()
+//{ 
+//	int res,res1;
+//	while(1)
+//	{
+//		res=read(resfd,rdata,sizeof(struct data));
+//		printf("Server Processed data first =%d,second=%d and operator=%c and result=%d\n",rdata->first,rdata->second,rdata->operator_name,rdata->result);
+//		//open client fifo with the process id in its name
+//		sprintf(client_fifo,OP_CLIENT_FIFO,rdata->pid);
+//		client_fifo_fd=open(client_fifo,O_WRONLY);
+//		res1=read(client_fifo_fd,rdata,sizeof(struct data));
+//		//(void)close(reqfd);
+//	}
+//}
 int main()
 {
 
-        int pipefd;
-        int pipefd1[2];
-        //char buffer[BUFFER_SIZE+1];
-	int nbytes,stat,res,bytes_read,result=0;
-	struct data *rdata;
-        bytes_read=0;
-        rdata=malloc(sizeof(struct data));
+	int *result[100];
+	//char buffer[BUFFER_SIZE+1];
+	int stat,res,res1=0;
+	rdata=malloc(sizeof(struct data));
 	memset(rdata,'\0',sizeof(struct data));
-        if (access(REQFIFO,F_OK)==-1)
+	if (access(REQFIFO,F_OK)==-1)
 	{
-	        stat=mkfifo(REQFIFO,0777);
-        	perror("rq_fifo");
-                if(stat!=0)
+		stat=mkfifo(REQFIFO,0777);
+		if(stat!=0)
 		{
-			fprintf(stderr,"fifo not created\n");
-                        exit(EXIT_FAILURE);
+			perror("rq_fifo");
+			exit(EXIT_FAILURE);
 		}
 	}
-        if (access(PROCESSINGFIFO,F_OK)==-1)
+//	if (access(PXFIFO,F_OK)==-1)
+//	{
+//		stat=mkfifo(PXFIFO,0777);
+//		if(stat!=0)
+//		{
+//			perror("px_fifo");
+//			exit(EXIT_FAILURE);
+//		}
+//	}
+//	if (access(RESFIFO,F_OK)==-1)
+//	{
+//		stat=mkfifo(RESFIFO,0777);
+//		if(stat!=0)
+//		{
+//			perror("res_fifo");
+//			exit(EXIT_FAILURE);
+//		}
+//	}
+	//reqfd=open(REQFIFO,O_RDONLY);
+	reqfd=open(REQFIFO,O_RDWR);
+//	pxfd=open(PXFIFO,O_WRONLY);
+	printf("Server Process id =%d,reqfd=%d\n",getpid(),reqfd);
+	if (reqfd ==-1) 
 	{
-	        stat=mkfifo(PXFIFO,0777);
-        	perror("px_fifo");
-                if(stat!=0)
-		{
-			fprintf(stderr,"fifo not created\n");
-                        exit(EXIT_FAILURE);
-		}
+		perror("reqfd and pxfd open");
+		exit(EXIT_FAILURE);
 	}
-        reqfd=open(REQFIFO,O_RDONLY|O_NONBLOCK);
-        pxfd=open(PXFIFO,O_RDWR);
-        if ((reqfd && pxfd)!=-1)
-        {
-                while(1)
-                {
+	else
+	{
+		pthread_create( &thread1, NULL, &read_req, NULL);
+        	perror("server read_req thread\n");
+	}
+     pthread_join( thread1, NULL);
 
-	  		res=read(reqfd,rdata,sizeof(struct data));
-  			res1=write(pxfd,rdata,BUFFER_SIZE);
-
-        		(void)close(pipefd);
-		}
-	}
-        else
-        {
-                exit(EXIT_FAILURE);
-	
-	}
-         
-        exit(EXIT_SUCCESS);
+ 
+//	resfd=open(RESFIFO,O_RDONLY);
+//	//while(1)
+//	if (resfd ==-1)
+//	{
+//		perror("reqfd and pxfd open");
+//		exit(EXIT_FAILURE);
+//	}
+//	else
+//	{
+//		pthread_create( &thread2, NULL, &read_res, NULL);
+//	}
+	exit(EXIT_SUCCESS);
 }
